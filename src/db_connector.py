a1c8338eed86adf96b1bc9f247030866a898064a
Stanislav Kucherenko <svkwork1@gmail.com>
2025-11-08 00:34:04 +0100

    fixes problem with temporary db

--- src/db_connector.py
+++ src/db_connector.py
@@ -1,10 +1,14 @@
 # Connector for SQLite DB
 import sqlite3
 from sqlite3 import Connection
+import os
 
-DB_PATH_MAIN = './db.db'
+# Use /tmp for the database path, as it's a writable in-memory filesystem in Cloud Run
+DB_PATH_MAIN = '/tmp/db.db'
 
 def get_db_connection(db_path: str) -> Connection:
+    # Ensure the directory exists
+    os.makedirs(os.path.dirname(db_path), exist_ok=True)
     conn = sqlite3.connect(db_path)
     conn.row_factory = sqlite3.Row
     return conn
